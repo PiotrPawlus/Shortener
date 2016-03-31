fdf6e409f3de18af8cdca35e0145d26544b596bf
Piotr Pawluś <pawlus.piotrek@gmail.com>
2016-03-31 14:44:16 +0200

    create Entity with NSManagedObject class

---
+++ Shortener/Shortener.swift
@@ -0,0 +1,18 @@
+//
+//  Shortener.swift
+//  Shortener
+//
+//  Created by Piotr Pawluś on 31/03/16.
+//  Copyright © 2016 Piotr Pawluś. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+
+class Shortener: NSManagedObject {
+
+    @NSManaged var httpAddress: String
+    @NSManaged var shortHttpAddress: String
+    
+}
